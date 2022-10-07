88a0031ec3ed530324c0a1040d05c8ee32be0b40
Fhilipe Coelho <fhilipecoelho.dev@gmail.com>
2022-10-07 20:57:28 -0300

    `feat`: add login with jwt token

---
+++ src/main/java/com/fhilipecrash/usersposts/controllers/AuthController.java
@@ -0,0 +1,53 @@
+package com.fhilipecrash.usersposts.controllers;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.fhilipecrash.usersposts.models.User;
+import com.fhilipecrash.usersposts.services.user.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+class AuthRequest {
+    public String email;
+    public String password;
+}
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/login")
+    @ResponseStatus(code = HttpStatus.OK)
+    public Map<String, Object> login(@RequestBody AuthRequest user) {
+        User loginUser = userService.getUserByEmail(user.email);
+
+        if (loginUser == null) {
+            throw new RuntimeException("User not found");
+        }
+
+        if (!loginUser.getPassword().equals(user.password)) {
+            throw new RuntimeException("Invalid password");
+        }
+
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(user.password);
+            String token = JWT.create().withIssuer("auth0").sign(algorithm);
+
+            Map<String, Object> response = new LinkedHashMap<>();
+            response.put("user", loginUser);
+            response.put("token", token);
+
+            return response;
+        } catch (JWTCreationException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
